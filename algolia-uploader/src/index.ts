2c57c8caa9a4f3ac14e4b20e676837946f7c5baa
sthairno <45668272+sthairno@users.noreply.github.com>
2025-07-15 19:55:57 +0900

    バッチのサイズを500に減らしてみる

--- algolia-uploader/src/index.ts
+++ algolia-uploader/src/index.ts
@@ -38,6 +38,7 @@ async function main() {
     waitForTasks: true,
     createIfNotExists: true,
     objects: chunks,
+    batchSize: 500,
   });
 
   console.log(`Deleting the old chunks from ${algoliaIndexName}`);
