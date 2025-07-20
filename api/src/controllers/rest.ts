fef4a81babf8348678f3ae39c4c181ae747135bc
sthairno <45668272+sthairno@users.noreply.github.com>
2025-07-20 12:28:12 +0900

    /docs/markdownエンドポイントの実装

--- api/src/controllers/rest.ts
+++ api/src/controllers/rest.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { structuredSearch } from "../lib/algolia";
-import { getDocsCodeblock } from "../lib/storage";
+import { getDocsCodeblock, getDocsMarkdown } from "../lib/storage";
 import { zValidator } from "@hono/zod-validator";
 import z from "zod";
 
@@ -27,6 +27,39 @@ rest.get(
     return c.text(result, 200, {
       "Content-Type": "text/markdown; charset=utf-8",
     });
+  }
+);
+
+const readMarkdownSchema = z.object({
+  url: z.string().url(),
+});
+
+rest.get(
+  "/docs/markdown",
+  zValidator("query", readMarkdownSchema, (result, c) => {
+    if (!result.success) {
+      return c.text("url and lang are both required", 400);
+    }
+    if (!result.data.url.startsWith("https://siv3d.github.io/")) {
+      return c.text("url must start with https://siv3d.github.io/", 400);
+    }
+  }),
+  async (c) => {
+    const query = c.req.valid("query");
+    const parsedUrl = new URL(query.url);
+
+    const route = parsedUrl.pathname.split("/").slice(1);
+    if (route.at(-1) === "") {
+      route.pop();
+    }
+
+    const markdown = await getDocsMarkdown(route);
+
+    return markdown
+      ? c.text(markdown, 200, {
+          "Content-Type": "text/markdown; charset=utf-8",
+        })
+      : c.text("Not Found", 404);
   }
 );
 
